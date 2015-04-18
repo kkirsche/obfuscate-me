faabf63bc2aa898bd3a3d0a8f999bf310dd4aac0
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-04-18 16:59:10 -0400

    Add a test for shorter visa cards

--- test/obfuscate_my/credit_cards/visa_spec.rb
+++ test/obfuscate_my/credit_cards/visa_spec.rb
@@ -20,6 +20,19 @@ module TestObfuscateMy
         visa.security_code.must_equal '012'
       end
 
+      it 'should initialize and correctly assign values for older 13 digit cards.' do
+        visa = ObfuscateMy::Visa.new number: '4222222222222',
+                                     exp_year: 9999, exp_month: 01,
+                                     name: 'Kevin Kirsche', security_code: '012'
+
+        assert_kind_of ObfuscateMy::Visa, visa
+        visa.type.must_equal :visa
+        visa.cardholder_name.must_equal 'Kevin Kirsche'
+        visa.number.must_equal '4222222222222'
+        visa.expiration_date.must_equal Time.new 9999, 01
+        visa.security_code.must_equal '012'
+      end
+
       it 'should raise an error when it incorrectly is instantiated.' do
         assert_raises RuntimeError do
           visa = ObfuscateMy::Visa.new number: '',
